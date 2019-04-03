1fd3b85f756e798d4fb592a23b286b2bdc206e40
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-03 20:45:06 +0200

    Exposing more Table options

--- Public/Table.ps1
+++ Public/Table.ps1
@@ -2,7 +2,28 @@ function Table {
     [CmdletBinding()]
     param(
         [Array] $DataTable,
-        [switch] $HideFooter
+        [string[]][ValidateSet('copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5')] $Buttons = @('copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5'),
+        [string[]][ValidateSet('numbers', 'simple', 'simple_numbers', 'full', 'full_numbers', 'first_last_numbers')] $PagingStyle = 'full_numbers',
+        [int[]]$PagingOptions = @(15, 25, 50, 100),
+        [switch]$DisablePaging,
+        [switch]$DisableOrdering,
+        [switch]$DisableInfo,
+        [switch]$HideFooter,
+        [switch]$DisableColumnReorder,
+        [switch]$DisableProcessing,
+        [switch]$DisableResponsiveTable,
+        [switch]$DisableSelect,
+        [switch]$DisableStateSave,
+        [switch]$DisableSearch,
+        [bool] $ScrollCollapse,
+        [string[]][ValidateSet('display', 'cell-border', 'compact', 'hover', 'nowrap', 'order-column', 'row-border', 'stripe')] $Style = @('display', 'compact'),
+        [string]$TextWhenNoData = 'No data available.',
+        [int] $ScreenSizePercent = 0
     )
-    New-HTMLTable -DataTable $DataTable -HideFooter:$HideFooter
+    New-HTMLTable -DataTable $DataTable `
+        -HideFooter:$HideFooter `
+        -Buttons $Buttons -PagingStyle $PagingStyle -PagingOptions $PagingOptions `
+        -DisablePaging:$DisablePaging -DisableOrdering:$DisableOrdering -DisableInfo:$DisableInfo -DisableColumnReorder:$DisableColumnReorder -DisableProcessing:$DisableProcessing `
+        -DisableResponsiveTable:$DisableResponsiveTable -DisableSelect:$DisableSelect -DisableStateSave:$DisableStateSave -DisableSearch:$DisableSearch -ScrollCollapse $ScrollCollapse `
+        -Style $Style -TextWhenNoData:$TextWhenNoData -ScreenSizePercent $ScreenSizePercent
 }
