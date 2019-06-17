1fbb225e2c64cd439a41a466f7199c3aab2274bf
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-17 12:18:00 +0200

    Update

--- Public/Panel.ps1
+++ Public/Panel.ps1
@@ -2,7 +2,7 @@ function Panel {
     param(
         [Parameter(Position = 0)][ValidateNotNull()][ScriptBlock] $Content = $(Throw "Panel requires opening and closing brace."),
         [string] $Name,
-        [alias('BackgroundShade')][RGBColors]$BackgroundColor,
+        [alias('BackgroundShade')][RGBColors]$BackgroundColor = [RGBColors]::None,
         [switch] $Invisible
     )
     New-HTMLPanel -Invisible:$Invisible -BackgroundColor $BackgroundColor {
