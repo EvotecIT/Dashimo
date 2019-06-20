45f371d27817e44bb47e91ec6fd404181ca8d817
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-20 15:14:57 +0200

    Example update

--- Example/Run-SimplestTable01-DefaultSort.ps1
+++ Example/Run-SimplestTable01-DefaultSort.ps1
@@ -3,4 +3,8 @@ $Process = Get-Process | Select-Object -First 30
 
 Dashboard -Name 'Dashimo Test' -FilePath $PSScriptRoot\Output\DashboardEasy01.html -AutoRefresh 15 -Show {
     Table -DataTable $Process -DefaultSortIndex 4 -ScrollCollapse -HideFooter -Buttons @()
+
+    Table -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -HideFooter -Buttons @()
+
+    Table -DataTable $Process -DefaultSortColumn 'WorkingSet' -DefaultSortOrder Descending -ScrollCollapse -HideFooter -Buttons @()
 }
