56ff64ee59ec843f57483fd1c5db62fc0b7087a5
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-28 22:58:53 +0200

    Moved Dashimo into PSWriteHTML project. Archiving this project.

--- Public/Container.ps1
+++
@@ -1,7 +0,0 @@
-﻿function Container {
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0)][ValidateNotNull()][ScriptBlock] $Content = $(Throw "Container requires opening and closing brace.")
-    )
-    New-HTMLContainer -HTML $Content
-}
